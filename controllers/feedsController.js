f3516a3182cb19eab5840a2c8e9af4317bc23b9c
DrFemzy <adesokanemmanuelfemzy2018@gmail.com>
2023-03-30 21:42:53 +0100

    Fixed bugs with Headers

--- controllers/feedsController.js
+++ controllers/feedsController.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { removeHeaders } = require("../utils/headers");
 
 const autoComplete = (req, res) => {
     const options = {
@@ -15,19 +14,11 @@ const autoComplete = (req, res) => {
 
 
     axios.get("https://yummly2.p.rapidapi.com/feeds/auto-complete", options)
-        .then((response) => {
-            // Remove the extra Headers
-            removeHeaders(res)
-            
+        .then((response) => {            
             // Response details and status
-            res.set(response.headers)
             res.status(response.status).json(response.data)
         })
         .catch((err) => {
-            // removeHeaders(res) // remove the extra headers
-            removeHeaders(res)
-            // The Error response details and status
-            res.set(err.response?.headers)
             res.status(err.response?.status).json(err.response?.data)
         })
 }
@@ -46,19 +37,9 @@ const search = (req, res) => {
 
     axios.get("https://yummly2.p.rapidapi.com/feeds/search", options)
         .then((response) => {
-
-            // Remove the extra Headers
-            removeHeaders(res)
-            
-            // Response details and status
-            res.set(response.headers)
             res.status(response.status).json(response.data)
         })
         .catch((err) => {
-            removeHeaders(res) // remove the extra headers
-
-            // The Error response details and status
-            res.set(err.response?.headers)
             res.status(err.response?.status).json(err.response?.data)
         })
 }
@@ -77,19 +58,9 @@ const list = (req, res) => {
 
     axios.get("https://yummly2.p.rapidapi.com/feeds/list", options)
         .then((response) => {
-
-            // Remove the extra Headers
-            removeHeaders(res)
-            
-            // Response details and status
-            res.set(response.headers)
             res.status(response.status).json(response.data)
         })
         .catch((err) => {
-            removeHeaders(res) // remove the extra headers
-
-            // The Error response details and status
-            res.set(err.response?.headers)
             res.status(err.response?.status).json(err.response?.data)
         })
 }
@@ -108,19 +79,9 @@ const listSimilarities = (req, res) => {
 
     axios.get("https://yummly2.p.rapidapi.com/feeds/list-similarities", options)
         .then((response) => {
-
-            // Remove the extra Headers
-            removeHeaders(res)
-            
-            // Response details and status
-            res.set(response.headers)
             res.status(response.status).json(response.data)
         })
         .catch((err) => {
-            removeHeaders(res) // remove the extra headers
-
-            // The Error response details and status
-            res.set(err.response?.headers)
             res.status(err.response?.status).json(err.response?.data)
         })
 }
--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { removeHeaders } = require("../utils/headers");
 
 const list = (req, res) => {
     const options = {
@@ -15,19 +14,9 @@ const list = (req, res) => {
 
     axios.get("https://yummly2.p.rapidapi.com/reviews/list", options)
         .then((response) => {
-
-            // Remove the extra Headers
-            removeHeaders(res)
-            
-            // Response details and status
-            res.set(response.headers)
             res.status(response.status).json(response.data)
         })
         .catch((err) => {
-            removeHeaders(res) // remove the extra headers
-
-            // The Error response details and status
-            res.set(err.response?.headers)
             res.status(err.response?.status).json(err.response?.data)
         })
 }
